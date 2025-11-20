b3e560c4a5a2953f83670b01abf883254747ee62
Jonas Almaas <jonas.almaas@siemens-energy.com>
2025-11-20 20:19:43 +0100

    Neovim synbols

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,17 @@ return {
 		'hrsh7th/cmp-cmdline',
 		'hrsh7th/nvim-cmp',
 		'j-hui/fidget.nvim',
+		{
+			"folke/lazydev.nvim",
+			ft = "lua", -- only load on lua files
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
 	},
 
 	config = function()

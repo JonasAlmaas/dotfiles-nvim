b2c8869462b926d4a1a9ab51600d0e9bf4618a5b
Jonas Almaas <jonas.almaas@siemens-energy.com>
2025-11-20 17:11:08 +0100

    Include untracked files in find project files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
 		local builtin = require('telescope.builtin');
 
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Telescope find files'});
-		vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = 'Telescope git files'});
+		vim.keymap.set('n', '<C-p>', function() builtin.git_files({show_untracked=true}); end, {desc = 'Telescope git files',});
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Telescope live grep'});
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'});
 

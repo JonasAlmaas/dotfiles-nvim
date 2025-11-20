ca1abff14312fcebb6c381df81da290e4e9094ef
Jonas Almaas <jonas.almaas@siemens-energy.com>
2025-11-20 16:36:39 +0100

    Remaps ++

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,4 +2,29 @@ vim.g.mapleader = ' ';
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex);
 
+-- Allow moving blocks of code in visual mode
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv");
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv");
+
+-- Keep cursor centered when half page jumping
+vim.keymap.set('n', '<C-d>', '<C-d>zz');
+vim.keymap.set('n', '<C-u>', '<C-u>zz');
+-- Keep cursor centered when jumping to next search result
+vim.keymap.set('n', 'n', 'nzzzv');
+vim.keymap.set('n', 'N', 'Nzzzv');
+
+-- Paste without overwriting the buffer
 vim.keymap.set('x', '<leader>p', [["_dP]]);
+
+-- Yank to system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>y', [["+y]]);
+vim.keymap.set('n', '<leader>Y', [["+Y]]);
+
+-- Delete to void register
+vim.keymap.set({'n', 'v'}, '<leader>d', [["_d]]);
+
+-- Unbind Ex mode switch
+vim.keymap.set('n', 'Q', '<nop>');
+
+-- Replace hovered word
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]);
--- lua/set.lua
+++ lua/set.lua
@@ -1,3 +1,15 @@
+vim.opt.nu = true; -- Show current line number
 vim.opt.relativenumber = true;
 
+vim.opt.smartindent = true;
+
 vim.opt.wrap = false;
+
+vim.opt.hlsearch = false;
+vim.opt.incsearch = true;
+
+vim.opt.scrolloff = 8; -- Always keep 8 lines of margin around cursor
+
+vim.opt.updatetime = 50;
+
+vim.opt.colorcolumn = "80"

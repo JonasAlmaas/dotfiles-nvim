d2aa3c5a876a442b984cfbed2366b750702a8d45
Jonas Almaas <jonas.almaas@siemens-energy.com>
2025-11-20 17:14:39 +0100

    Undo tree

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,10 @@
+return {
+	'mbbill/undotree',
+
+	config = function() 
+		-- 'diff' is not a default application on Windows
+		vim.g.undotree_DiffCommand = "FC";
+
+		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle);
+	end
+}

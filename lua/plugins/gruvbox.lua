553d0f28274e4f721685d01b826d4cd035a6f3be
Jonas Almaas <jonas.almaas@siemens-energy.com>
2025-11-20 18:08:10 +0100

    I dont like italics in code

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,5 +1,17 @@
 return {
 	"ellisonleao/gruvbox.nvim",
 	priority = 1000,
-	config = true
+	config = function()
+		local gruvbox = require('gruvbox');
+
+		gruvbox.setup({
+			italic = {
+				strings = false,
+				emphasis = false,
+				comments = false,
+				operators = false,
+				folds = false,
+			}
+		});
+	end
 }

dc3f774f379a6c87a6547ee36b5e3d10221973f8
Arild Hystad <arild.hystad@gmail.com>
2025-11-21 12:30:13 +0100

    m

--- lua/remap.lua
+++ lua/remap.lua
@@ -32,7 +32,7 @@ vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- Useful keymap for editing configs
 vim.keymap.set('n', '<leader><leader>', function() vim.cmd([[so]]); end);
 
--- Only remap if an lsp is present
+-- Only remap if an LSP is present
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(e)
 		local opts = { buffer = e.buf }

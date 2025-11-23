916f3dd5fd99329a0c1219d5a7b89f026bbfb99a
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-11-23 15:17:27 +0100

    Persistent undo tree

--- lua/set.lua
+++ lua/set.lua
@@ -8,15 +8,18 @@ vim.opt.wrap = false;
 vim.opt.hlsearch = false;
 vim.opt.incsearch = true;
 
+vim.opt.swapfile = false;
+vim.opt.backup = false;
+vim.opt.undodir = vim.fn.expand('$HOME/.vim/undodir');
+vim.opt.undofile = true;
+
 vim.opt.scrolloff = 8; -- Always keep 8 lines of margin around cursor
+vim.opt.signcolumn = 'yes'; -- Always show signal column (Prevent flickering when entering and exiting insert mode)	
 
 vim.opt.updatetime = 50;
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "80" -- Guideline
 
 -- Render whitespace
 vim.o.list = true
 vim.o.listchars = 'tab:» ,lead:·,trail:·'
-
--- Always show signal column (Prevent flickering when entering and exiting insert mode)
-vim.o.signcolumn = 'yes';

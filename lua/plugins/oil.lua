f95c305bb771f8e6883d082d351897e632a50e56
Jonas Almaas <jonas.almaas@siemens-energy.com>
2025-11-24 12:39:48 +0100

    Oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,4 +4,14 @@ return {
 	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	lazy = false,
+	config = function()
+		require('oil').setup({
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, bufno)
+					return name == '.git';
+				end,
+			}
+		});
+	end
 };

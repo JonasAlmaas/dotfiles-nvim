be5d98eba0b541f7821d4d9f9bf9b7faff24ceff
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-11-25 22:49:30 +0100

    Floaterm cleanup

--- lua/floaterm.lua
+++ lua/floaterm.lua
@@ -4,11 +4,9 @@ local state = {
 	curr_bufix = -1,
 };
 
-local draw_tabline = function(winid)
-	if not vim.api.nvim_win_is_valid(winid) then return "" end
-
+local draw_tabline = function()
 	local s = "%#StatusLineTermNC#"
-	for i in pairs(state.bufids) do
+	for i=1, #state.bufids do
 		if i == state.curr_bufix then
 			s = s .. "%#StatusLineTerm# [" .. i .. "] %#StatusLineTermNC#"
 		else
@@ -20,25 +18,16 @@ local draw_tabline = function(winid)
 	return s;
 end
 
-local new_buf = function()
-	return vim.api.nvim_create_buf(false, true); -- No file, scratch buffer
-end
-
 local create_floating_window = function(opts)
 	opts = opts or {};
+
+	local bufid = opts.bufid or 0;
 
 	local width = opts.width or math.floor(vim.o.columns * 0.8);
 	local height = opts.height or math.floor(vim.o.lines * 0.8);
 
 	local col = math.floor((vim.o.columns - width) / 2);
 	local row = math.floor((vim.o.lines - height) / 2);
-
-	local bufid = nil;
-	if vim.api.nvim_buf_is_valid(opts.buf) then
-		bufid = opts.buf;
-	else
-		bufid = new_buf();
-	end
 
 	local win_conf = {
 		relative = 'editor',
@@ -52,31 +41,32 @@ local create_floating_window = function(opts)
 
 	local winid = vim.api.nvim_open_win(bufid, true, win_conf);
 
-	vim.api.nvim_set_option_value('winbar', draw_tabline(winid), {win=winid});
+	vim.api.nvim_set_option_value('winbar', draw_tabline(), {win=winid});
 
 	vim.api.nvim_set_hl(0, 'FloatBorder', {bg='NONE'});
 	vim.wo[winid].winhighlight = 'Normal:Normal';
 
-	return {bufid=bufid, winid=winid};
+	return winid;
 end
 
 local open_terminal = function()
+	if vim.api.nvim_win_is_valid(state.winid) then
+		return;
+	end
+
 	if state.curr_bufix == -1 then
 		state.curr_bufix = 1;
-		state.bufids[state.curr_bufix] = new_buf();
+		state.bufids[state.curr_bufix] = vim.api.nvim_create_buf(false, true);
 	end
 
-	if not vim.api.nvim_win_is_valid(state.winid) then
-		local new_state = create_floating_window({buf = state.bufids[state.curr_bufix]});
+	local bufid = state.bufids[state.curr_bufix];
 
-		if vim.bo[new_state.bufid].buftype ~= 'terminal' then
-			vim.cmd.terminal();
-		end
-		vim.cmd('normal! i') -- Enter Terminal Job mode
+	state.winid = create_floating_window({bufid=bufid});
 
-		state.winid = new_state.winid;
-		state.bufids[state.curr_bufix] = new_state.bufid;
+	if vim.bo[bufid].buftype ~= 'terminal' then
+		vim.cmd.terminal();
 	end
+	vim.cmd('normal! i') -- Enter Terminal Job mode
 end
 
 local close_terminal = function()
@@ -97,20 +87,20 @@ local open_curr_buf = function()
 	local bufid = state.bufids[state.curr_bufix];
 
 	vim.api.nvim_win_set_buf(state.winid, bufid);
-	vim.api.nvim_set_option_value('winbar', draw_tabline(state.winid), {win=state.winid});
+	vim.api.nvim_set_option_value('winbar', draw_tabline(), {win=state.winid});
 
 	if vim.bo[bufid].buftype ~= 'terminal' then
 		vim.cmd.terminal();
 	end
 end
 
-local create_new_buf = function()
+local new_buf = function()
 	if not vim.api.nvim_win_is_valid(state.winid) then
 		return;
 	end
 
 	state.curr_bufix = #state.bufids + 1;
-	state.bufids[state.curr_bufix] = new_buf();
+	state.bufids[state.curr_bufix] = vim.api.nvim_create_buf(false, true);
 
 	open_curr_buf();
 end
@@ -127,7 +117,7 @@ local delete_buf = function()
 
 	if state.curr_bufix < 1 then
 		state.curr_bufix = 1;
-		state.bufids[state.curr_bufix] = new_buf()
+		state.bufids[state.curr_bufix] = vim.api.nvim_create_buf(false, true);
 	end
 
 	open_curr_buf();
@@ -144,7 +134,7 @@ local delete_all_bufs = function()
 
 	state.bufids = {};
 	state.curr_bufix = 1;
-	state.bufids[state.curr_bufix] = new_buf();
+	state.bufids[state.curr_bufix] = vim.api.nvim_create_buf(false, true);
 
 	open_curr_buf();
 
@@ -182,7 +172,7 @@ end
 local goto_buf = function(bufix)
 	if bufix > #state.bufids then
 		for i=#state.bufids, bufix do
-			state.bufids[i] = new_buf();
+			state.bufids[i] = vim.api.nvim_create_buf(false, true);
 		end
 	end
 
@@ -198,13 +188,14 @@ end
 vim.api.nvim_create_user_command('FloatermOpen', open_terminal, {});
 vim.api.nvim_create_user_command('FloatermClose', close_terminal, {});
 vim.api.nvim_create_user_command('FloatermToggle', toggle_terminal, {});
-vim.api.nvim_create_user_command('FloatermCreatebuf', create_new_buf, {});
+vim.api.nvim_create_user_command('FloatermNewbuf', new_buf, {});
 vim.api.nvim_create_user_command('FloatermDeleteBuf', delete_buf, {});
+vim.api.nvim_create_user_command('FloatermDeleteAllBufs', delete_all_bufs, {});
 vim.api.nvim_create_user_command('FloatermNextBuf', next_buf, {});
 vim.api.nvim_create_user_command('FloatermPrevBuf', prev_buf, {});
 
 vim.keymap.set({'n', 't'}, '<leader>tt', toggle_terminal);
-vim.keymap.set({'t'}, '<leader>tc', create_new_buf, {});
+vim.keymap.set({'t'}, '<leader>tc', new_buf, {});
 vim.keymap.set({'t'}, '<leader>td', delete_buf);
 vim.keymap.set({'t'}, '<leader>tda', delete_all_bufs);
 vim.keymap.set({'t'}, '<leader>tn', next_buf);

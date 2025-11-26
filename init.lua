5cf21792a2139f31cd3978bf56be6c17ab4505a4
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-11-26 18:49:46 +0100

    Highlight on yank

--- init.lua
+++ init.lua
@@ -8,3 +8,11 @@ require('floaterm');
 --vim.cmd('colorscheme gruvbox');
 --vim.cmd('colorscheme kanagawa-dragon');
 vim.cmd.colorscheme("oasis");
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	group = vim.api.nvim_create_augroup('HighlightYank', {}),
+	pattern = '*',
+	callback = function()
+		vim.highlight.on_yank();
+	end,
+});

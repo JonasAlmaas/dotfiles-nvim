a79b07e9ddf4c25266023248edc54bfc73ad3bdb
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-11-26 18:36:09 +0100

    Window navigation

--- lua/remap.lua
+++ lua/remap.lua
@@ -3,6 +3,12 @@ vim.g.mapleader = ' ';
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex);
 vim.keymap.set('n', '<leader>e', function() vim.cmd(':Oil'); end);
 
+-- Easy window navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>');
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>');
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>');
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>');
+
 -- Allow moving blocks of code in visual mode
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv");
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv");

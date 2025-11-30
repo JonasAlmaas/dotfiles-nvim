37d67781b432276acf430b6547a37f2d09ce0498
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-11-30 15:55:21 +0100

    Harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,24 +1,53 @@
 return {
 	'ThePrimeagen/harpoon',
+	branch = 'harpoon2',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		--'nvim-telescope/telescope.nvim',
+	},
 	config = function()
-		local mark = require("harpoon.mark");
-		local ui = require("harpoon.ui");
+		local harpoon = require("harpoon");
 
-		vim.keymap.set("n", "<leader>a", mark.add_file);
-		--vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu);
-		vim.keymap.set("n", "<leader>hh", ui.toggle_quick_menu);
+		harpoon:setup({});
 
-		vim.keymap.set("n", "<leader>ha", function() ui.nav_file(1) end);
-		vim.keymap.set("n", "<leader>hs", function() ui.nav_file(2) end);
-		vim.keymap.set("n", "<leader>hd", function() ui.nav_file(3) end);
-		vim.keymap.set("n", "<leader>hf", function() ui.nav_file(4) end);
+		-- Keymapping
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end);
+		--vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end);
+		vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end);
 
-		vim.keymap.set("n", "<leader>h1", function() ui.nav_file(1) end);
-		vim.keymap.set("n", "<leader>h2", function() ui.nav_file(2) end);
-		vim.keymap.set("n", "<leader>h3", function() ui.nav_file(3) end);
-		vim.keymap.set("n", "<leader>h4", function() ui.nav_file(4) end);
+		vim.keymap.set("n", "<leader>ha", function() harpoon:list():select(1) end);
+		vim.keymap.set("n", "<leader>hs", function() harpoon:list():select(2) end);
+		vim.keymap.set("n", "<leader>hd", function() harpoon:list():select(3) end);
+		vim.keymap.set("n", "<leader>hf", function() harpoon:list():select(4) end);
 
-		vim.keymap.set("n", "<leader>hp", function() ui.nav_prev() end);
-		vim.keymap.set("n", "<leader>hn", function() ui.nav_next() end);
+		vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end);
+		vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end);
+		vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end);
+		vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end);
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end);
+		vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end);
+
+		-- Use telescope as the UI
+		--[[local conf = require("telescope.config").values;
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {};
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value);
+			end
+
+			require("telescope.pickers").new({}, {
+				prompt_title = "Harpoon",
+				finder = require("telescope.finders").new_table({
+					results = file_paths,
+				}),
+				previewer = conf.file_previewer({}),
+				sorter = conf.generic_sorter({}),
+			}):find();
+		end
+
+		--vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end);
+		vim.keymap.set("n", "<leader>hh", function() toggle_telescope(harpoon:list()) end);]]
 	end
 };

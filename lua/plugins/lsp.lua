5d3ce3f021556fa151ed9c480c70e8b662c093a6
Jonas Almaas <jonas.almaas@siemens-energy.com>
2026-01-21 00:26:20 +0100

    Add ctrl e as an option to open completion menu

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -55,7 +55,8 @@ return {
 					['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 					['<C-y>'] = cmp.mapping.confirm({ select = true }),
 					['<Tab>'] = cmp.mapping.confirm({ select = true }),
-					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-Space>'] = cmp.mapping.complete(), -- Does not work in Windows terminal by default
+					['<C-e>'] = cmp.mapping.complete(),
 				}),
 				sources = cmp.config.sources(
 					{ { name = 'nvim_lsp' } },

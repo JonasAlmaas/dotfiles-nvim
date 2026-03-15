0b178a5370149b4f7d3a21a9832c281384a50c96
Jonas Almaas <jonas.f.almaas@gmail.com>
2026-03-15 18:40:01 +0100

    m

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -43,8 +43,7 @@ return {
 						end
 
 						local max_filesize = 100 * 1024 -- 100 KB
-						---@diagnostic disable-next-line: undefined-field
-						local ok, stats = pcall(vim.loop.fs_tat, vim.api.nvim_buf_get_name(buf))
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 						if ok and stats and stats.size > max_filesize then
 							vim.notify(
 								"File larger than 100KB treesitter disabled for performance",

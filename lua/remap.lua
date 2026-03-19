367acdc18ea15dfdd7559b0adf688aec679735b9
Hystad <arild.hystad@gmail.com>
2026-03-19 14:43:30 +0100

    Function keys for invoking Oil and floatterm

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = ' ';
 
 --vim.keymap.set('n', '<leader>pv', vim.cmd.Ex);
 vim.keymap.set('n', '<leader>e', function() vim.cmd(':Oil'); end);
+vim.keymap.set('n', '<F2>', function() vim.cmd(':Oil'); end);
+vim.keymap.set('i', '<F2>', function() vim.cmd(':Oil'); end);
 
 -- Easy window navigation
 --vim.keymap.set('n', '<C-h>', '<C-w><C-h>');
